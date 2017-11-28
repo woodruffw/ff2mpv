af3e14f2e3ff6be17999631cab8f220fd439df2f
kirill9000 <10385391+kirill9000@users.noreply.github.com>
2017-11-28 19:13:03 +0000

    Fix Firefox 52 ESR compatibility

--- ff2mpv.js
+++ ff2mpv.js
@@ -2,17 +2,13 @@ function ff2mpv(url) {
     browser.runtime.sendNativeMessage("ff2mpv", { url: url });
 }
 
-browser.menus.create({
+browser.contextMenus.create({
     id: "ff2mpv",
     title: "Play in MPV",
-    contexts: ["link"],
-    icons: {
-        "16": "icons/icon_16x16.png",
-        "32": "icons/icon_32x32.png",
-    },
+    contexts: ["link"]
 });
 
-browser.menus.onClicked.addListener((info, tab) => {
+browser.contextMenus.onClicked.addListener((info, tab) => {
     switch (info.menuItemId) {
         case "ff2mpv":
             ff2mpv(info.linkUrl);

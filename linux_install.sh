8248a5da52d01681f6a0fa88bbc1273a644034fb
Shane Kimble <summonholmes@protonmail.com>
2020-10-06 12:50:33 -0400

    Make requested changes

--- linux_install.sh
+++ linux_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Some environment path variables
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -6,16 +6,16 @@ MOZ_DEST="$HOME/.mozilla"
 JSON_DEST="$MOZ_DEST/native-messaging-hosts"
 OLD_PATH="/home/william/scripts/ff2mpv"
 
-# Case by case copying the JSON
-if [ -d "$JSON_DEST" ]; then
-  cp ff2mpv.json $JSON_DEST
-elif [ -d "$MOZ_DEST" ]; then
-  mkdir $JSON_DEST
-  cp ff2mpv.json $JSON_DEST
+# Copying the JSON
+if [ -d "$MOZ_DEST" ]; then
+  mkdir -p "$JSON_DEST"
+  cp ff2mpv.json "$JSON_DEST"
 else
   echo "Please initialize the .mozilla folder"\
   "by running Firefox at least once"
+  exit 1
 fi
 
 # Replace the path in the copied JSON file
-sed -i "s|$OLD_PATH|$CURRENT_DIR/ff2mpv.py|g" $JSON_DEST/ff2mpv.json
+sed -i -e "s|$OLD_PATH|$CURRENT_DIR/ff2mpv.py|g" "$JSON_DEST"/ff2mpv.json
+echo "Install successful!"

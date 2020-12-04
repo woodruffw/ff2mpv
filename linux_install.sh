b34c9fd5e4487a4c2aca0874f0a34fe7b679dcc4
Atemu <atemu.main@gmail.com>
2020-12-04 04:48:14 +0100

    Do JSON installation and string substitution in one step
    This way we don't need --in-place which is problematic on BSD sed

--- linux_install.sh
+++ linux_install.sh
@@ -9,12 +9,10 @@ OLD_PATH="/home/william/scripts/ff2mpv"
 # Copying the JSON
 if [[ -d "$MOZ_DEST" ]]; then
   mkdir -p "$JSON_DEST"
-  cp ff2mpv.json "$JSON_DEST"
+  # Replace the placeholder path in the JSON file and install it
+  sed -e "s|$OLD_PATH|$CURRENT_DIR/ff2mpv.py|g" ff2mpv.json > "$JSON_DEST"/ff2mpv.json
 else
   echo "Please initialize the .mozilla folder by running Firefox at least once"
   exit 1
 fi
-
-# Replace the path in the copied JSON file
-sed -i -e "s|$OLD_PATH|$CURRENT_DIR/ff2mpv.py|g" "$JSON_DEST"/ff2mpv.json
 echo "Install successful!"

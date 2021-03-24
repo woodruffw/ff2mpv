5b69576068e47017a44c7f5b9268c1073ca43d1d
William Woodruff <william@yossarian.net>
2021-03-24 18:33:30 -0400

    clients: smash /usr/local/bin into the PATH on macOS (#49)
    * clients: smash /usr/local/bin into the PATH on macOS
    
    Fixes #48.

--- ff2mpv.py
+++ ff2mpv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import os
 import platform
 import struct
 import sys
@@ -17,6 +18,17 @@ def main():
     # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#Closing_the_native_app
     if platform.system() == "Windows":
         kwargs["creationflags"] = subprocess.CREATE_BREAKAWAY_FROM_JOB
+
+    # HACK(ww): On macOS, graphical applications inherit their path from `launchd`
+    # rather than the default path list in `/etc/paths`. `launchd` doesn't include
+    # `/usr/local/bin` in its default list, which means that any installations
+    # of MPV and/or youtube-dl under that prefix aren't visible when spawning
+    # from, say, Firefox. The real fix is to modify `launchd.conf`, but that's
+    # invasive and maybe not what users want in the general case.
+    # Hence this nasty hack.
+    if platform.system() == "Darwin":
+        path = os.environ.get("PATH")
+        os.environ["PATH"] = f"/usr/local/bin:{path}"
 
     subprocess.Popen(args, **kwargs)
 

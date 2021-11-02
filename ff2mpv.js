76a025e07fd612db1457a549402c04cf75b96320
Benyamin Limanto <BLBENYAMIN9@GMAIL.COM>
2021-11-02 11:31:09 +0700

    Change mnemonics for windows OS

--- ff2mpv.js
+++ ff2mpv.js
@@ -9,24 +9,30 @@ function ff2mpv(url) {
     browser.runtime.sendNativeMessage("ff2mpv", { url: url }).catch(onError);
 }
 
-browser.contextMenus.create({
-    id: "ff2mpv",
-    title: "Play in MPV"  + ( !!browser.contextMenus.getTargetElement ? " (&V)" : "" ),
-    contexts: ["link", "image", "video", "audio", "selection", "frame"]
-});
-
-browser.contextMenus.onClicked.addListener((info, tab) => {
-    switch (info.menuItemId) {
-        case "ff2mpv":
-            /* These should be mutually exclusive, but,
-               if they aren't, this is a reasonable priority.
-            */
-            url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl;
-            if (url) ff2mpv(url);
-            break;
-    }
-});
-
-browser.browserAction.onClicked.addListener((tab) => {
-    ff2mpv(tab.url);
+// Make check on platform
+platform.then(function(i) {
+  var os = i.os;
+  // if it's windows then use mnemonics V, if not use W as default
+  var contextMenu = os == "win" ? "&V" : "V (&W)";
+  browser.contextMenus.create({
+      id: "ff2mpv",
+      title: "Play in MP"  + ( !!browser.contextMenus.getTargetElement ? contextMenu : "" ),
+      contexts: ["link", "image", "video", "audio", "selection", "frame"]
+  });
+  
+  browser.contextMenus.onClicked.addListener((info, tab) => {
+      switch (info.menuItemId) {
+          case "ff2mpv":
+              /* These should be mutually exclusive, but,
+                 if they aren't, this is a reasonable priority.
+              */
+              url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl;
+              if (url) ff2mpv(url);
+              break;
+      }
+  });
+  
+  browser.browserAction.onClicked.addListener((tab) => {
+      ff2mpv(tab.url);
+  });
 });

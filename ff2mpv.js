9b62fd3c3094e59a144d0c4c00b09f2a223a0458
William Woodruff <william@yossarian.net>
2021-11-14 17:06:27 -0500

    Feature windows hotkey (#66)
    * Update ff2mpv.js
    
    Changging it to V for now. Still thinking a way to change via settings on Firefox
    
    * Change mnemonics for windows OS
    
    * Adding browser platform, forgot to put the update
    
    * ff2mpv: cleanup
    
    * workflows/ci: bump ECMA version for js lint
    
    Co-authored-by: Benyamin Limanto <BLBENYAMIN9@GMAIL.COM>

--- ff2mpv.js
+++ ff2mpv.js
@@ -9,24 +9,32 @@ function ff2mpv(url) {
     browser.runtime.sendNativeMessage("ff2mpv", { url: url }).catch(onError);
 }
 
-browser.contextMenus.create({
-    id: "ff2mpv",
-    title: "Play in MPV"  + ( !!browser.contextMenus.getTargetElement ? " (&W)" : "" ),
-    contexts: ["link", "image", "video", "audio", "selection", "frame"]
-});
+async function getOS() {
+  return browser.runtime.getPlatformInfo().then((i) => i.os);
+}
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
-    switch (info.menuItemId) {
-        case "ff2mpv":
-            /* These should be mutually exclusive, but,
-               if they aren't, this is a reasonable priority.
-            */
-            url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl;
-            if (url) ff2mpv(url);
-            break;
-    }
-});
+getOS().then((os) => {
+  var contextMenu = os == "win" ? "&V" : "(&W)";
 
-browser.browserAction.onClicked.addListener((tab) => {
-    ff2mpv(tab.url);
+  browser.contextMenus.create({
+      id: "ff2mpv",
+      title: "Play in MPV " + ( !!browser.contextMenus.getTargetElement ? contextMenu : "" ),
+      contexts: ["link", "image", "video", "audio", "selection", "frame"]
+  });
+
+  browser.contextMenus.onClicked.addListener((info, tab) => {
+      switch (info.menuItemId) {
+          case "ff2mpv":
+              /* These should be mutually exclusive, but,
+                 if they aren't, this is a reasonable priority.
+              */
+              url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl;
+              if (url) ff2mpv(url);
+              break;
+      }
+  });
+
+  browser.browserAction.onClicked.addListener((tab) => {
+      ff2mpv(tab.url);
+  });
 });

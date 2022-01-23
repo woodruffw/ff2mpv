540a0b73f431920f51bc50c7905cd47a1d6d462b
William Woodruff <william@yossarian.net>
2022-01-23 15:03:06 -0500

    package-chromium: select the browser appropriately

--- package-chromium.sh
+++ package-chromium.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
+
 set -x
+
+function installed {
+  cmd=$(command -v "${1}")
+
+  [[ -n "${cmd}" ]] && [[ -f "${cmd}" ]]
+  return ${?}
+}
+
 cd "${0%/*}" || exit 1
+
+if installed chromium ; then
+  browser=chromium
+elif installed google-chrome ; then
+  browser=google-chrome
+else
+  >&2 echo "I need a copy of Google Chrome or Chromium to package with."
+  exit 1
+fi
 
 rm -f dist.crx
 mkdir -p dist/
@@ -10,7 +28,7 @@ done < dist.files
 
 # chromium generates dist.pem on first use
 if [ -f dist.pem ]; then
-  chromium --pack-extension=dist --pack-extension-key=dist.pem
+  "${browser}" --pack-extension=dist --pack-extension-key=dist.pem
 else
-  chromium --pack-extension=dist
+  "${browser}" --pack-extension=dist
 fi

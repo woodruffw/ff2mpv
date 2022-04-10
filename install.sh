eb3c318391daa550f9e99bb6c71153fcacf08649
Eduardo D Sanchez <eduardo.daniel.s.m@gmail.com>
2022-04-09 20:34:39 -0400

    feat: Use a single bash script for installation firefox and chrome (#81)

--- install.sh
+++ install.sh
@@ -4,34 +4,44 @@
 if [[ -n $1 ]]; then
   browser=$1
 else
-  # does not default to chromium; users need to specify the browser
+  # does not default; users need to specify the browser
   :
 fi
+
+# Common variables
+LINUX_NMH_DIR="NativeMessagingHosts"
+JSON_FILE="ff2mpv-chromium.json"
 
 select_browser() {
   trap 'echo; exit 1' INT
   case $browser in
   # https://developer.chrome.com/docs/apps/nativeMessaging/#native-messaging-host-location
   chromium)
-    linux_chromium_path="$HOME/.config/chromium"
-    mac_chromium_path="$HOME/Library/Application Support/Chromium"
+    linux_path="$HOME/.config/chromium"
+    mac_path="$HOME/Library/Application Support/Chromium"
     ;;
   chrome)
-    linux_chromium_path="$HOME/.config/google-chrome"
-    mac_chromium_path="$HOME/Library/Application Support/Google/Chrome"
+    linux_path="$HOME/.config/google-chrome"
+    mac_path="$HOME/Library/Application Support/Google/Chrome"
     ;;
   brave)
-    linux_chromium_path="$HOME/.config/BraveSoftware/Brave-Browser"
-    mac_chromium_path="$HOME/Library/Application Support/BraveSoftware/Brave-Browser"
+    linux_path="$HOME/.config/BraveSoftware/Brave-Browser"
+    mac_path="$HOME/Library/Application Support/BraveSoftware/Brave-Browser"
     ;;
   edge)
     # https://docs.microsoft.com/en-us/microsoft-edge/extensions-chromium/developer-guide/native-messaging#step-3---copy-the-native-messaging-host-manifest-file-to-your-system
-    linux_chromium_path="$HOME/.config/microsoft-edge"
-    mac_chromium_path="$HOME/Library/Application Support/Microsoft Edge"
+    linux_path="$HOME/.config/microsoft-edge"
+    mac_path="$HOME/Library/Application Support/Microsoft Edge"
+    ;;
+  firefox)
+    linux_path="$HOME/.mozilla"
+    mac_path="$HOME/Library/Application Support/Mozilla"
+    LINUX_NMH_DIR="native-messaging-hosts"
+    JSON_FILE="ff2mpv.json"
     ;;
   custom)
-    linux_chromium_path="$2"
-    mac_chromium_path="$2"
+    linux_path="$2"
+    mac_path="$2"
     ;;
   *)
     echo >&2 "Invalid option. Please select a valid browser: \"chrome\" \"chromium\" \"brave\""
@@ -47,12 +57,12 @@ select_browser "$@"
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 case "$(uname)" in
 Linux*)
-  CHR_DEST="$linux_chromium_path"
-  JSON_DEST="$CHR_DEST/NativeMessagingHosts"
+  BROWSER_DEST="$linux_path"
+  JSON_DEST="$BROWSER_DEST/$LINUX_NMH_DIR"
   ;;
 Darwin*)
-  CHR_DEST="$mac_chromium_path"
-  JSON_DEST="$CHR_DEST/NativeMessagingHosts"
+  BROWSER_DEST="$mac_path"
+  JSON_DEST="$BROWSER_DEST/NativeMessagingHosts"
   ;;
 *)
   echo "Unsupported OS, please follow the manual instructions in the wiki"
@@ -62,10 +72,10 @@ esac
 OLD_PATH="/home/william/scripts/ff2mpv"
 
 # Copying the JSON
-if [[ -d "$CHR_DEST" ]]; then
+if [[ -d "$BROWSER_DEST" ]]; then
   mkdir -p "$JSON_DEST"
   # Replace the placeholder path in the JSON file and install it
-  sed -e "s|$OLD_PATH|$CURRENT_DIR/ff2mpv.py|g" ff2mpv-chromium.json >"$JSON_DEST"/ff2mpv.json
+  sed -e "s|$OLD_PATH|$CURRENT_DIR/ff2mpv.py|g" "$JSON_FILE" >"$JSON_DEST"/ff2mpv.json
 else
   echo "Please start your browser at least once to generate the required directories"
   exit 1

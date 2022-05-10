0b9013593279c3712b88862b7b11d6c00955a4b5
eNV25 <env252525@gmail.com>
2022-05-10 10:39:07 +0400

    Update to browser polyfill 0.9.0

--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -sSLO https://unpkg.com/webextension-polyfill@0.8.0/dist/browser-polyfill.js
+curl -sSLO https://unpkg.com/webextension-polyfill@0.9.0/dist/browser-polyfill.js

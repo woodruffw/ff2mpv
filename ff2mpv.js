7f0eb17f70db23e1bb4d63ec1fde7f4178919c72
Eduardo D Sanchez <eduardo.daniel.s.m@gmail.com>
2023-11-19 10:46:26 -0500

    Feat/mpv profiles (#108)
    * feat: Add options menu for extension
    
    * feat: Add options.html to manifest and storage permissions
    
    * fix: delete option without id error
    
    * feat: Update native client ff2mpv.py
    
    * feat: Update ff2mpv.js to allow multiple context menus
    
    * refactor: Remove logs
    
    * refactor: Add new line at end of file
    
    * feat: Update ruby client to support options
    
    * refactor: Reduce unnecessary function calls
    
    * refactor: Trim lines and prevent sending empty arguments
    
    * fix: Prevent access to dead objects
    
    * refactor: Remove uuid wrapper function
    
    * refactor: Improve css handling for light and dark theme
    
    * feat: Add warning message on top of options page
    
    * refactor: Expand control flow on save/update
    
    * refactor: Prevent unnecesary operations when deleting a new profile

--- ff2mpv.js
+++ ff2mpv.js
@@ -1,40 +1,99 @@
+const contexts = ["link", "image", "video", "audio", "selection", "frame"];
+
 function onError(error) {
-    console.log(`${error}`);
+  console.log(`${error}`);
 }
 
-function ff2mpv(url) {
-    browser.tabs.executeScript({
-        code: "video = document.getElementsByTagName('video');video[0].pause();"
-    });
-    browser.runtime.sendNativeMessage("ff2mpv", { url: url }).catch(onError);
+function ff2mpv(url, options = []) {
+  browser.tabs.executeScript({
+    code: "video = document.getElementsByTagName('video');video[0].pause();"
+  });
+  browser.runtime.sendNativeMessage("ff2mpv", { url, options }).catch(onError);
 }
 
 async function getOS() {
   return browser.runtime.getPlatformInfo().then((i) => i.os);
 }
 
-getOS().then((os) => {
+async function getProfiles() {
+  return (await browser.storage.sync.get('profiles'))['profiles'] || [];
+};
+
+async function getOptions(id) {
+  const profiles = await getProfiles();
+  const profile = profiles.find(pf => pf.id === id);
+
+  // If profile, remove empty lines
+  return profile
+    ? profile.content.filter(line => !!line)
+    : [];
+}
+
+async function submenuClicked(info) {
+  switch (info.parentMenuItemId) {
+    case "ff2mpv":
+      /* These should be mutually exclusive, but,
+         if they aren't, this is a reasonable priority.
+      */
+      url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl;
+      if (url) {
+        const options = await getOptions(info.menuItemId);
+        ff2mpv(url, options);
+      }
+    break;
+  }
+}
+
+function createProfile(profile) {
+  browser.contextMenus.create({
+    parentId: "ff2mpv",
+    id: profile.id,
+    title: profile.name,
+    contexts,
+    onclick: submenuClicked,
+  })
+}
+
+function deleteProfile(menuItemId) {
+  browser.contextMenus.remove(menuItemId);
+}
+
+function updateProfile(profile) {
+  browser.contextMenus.update(profile.id, {
+    title: profile.name,
+  });
+}
+
+getOS().then(async (os) => {
   var title = os == "win" ? "Play in MP&V" : "Play in MPV (&W)";
 
   browser.contextMenus.create({
-      id: "ff2mpv",
-      title: title,
-      contexts: ["link", "image", "video", "audio", "selection", "frame"]
+    id: "ff2mpv",
+    title: "Profiles",
+    contexts,
   });
 
-  browser.contextMenus.onClicked.addListener((info, tab) => {
-      switch (info.menuItemId) {
-          case "ff2mpv":
-              /* These should be mutually exclusive, but,
-                 if they aren't, this is a reasonable priority.
-              */
-              url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl;
-              if (url) ff2mpv(url);
-              break;
-      }
+  // Default entry
+  browser.contextMenus.create({
+    parentId: "ff2mpv",
+    title,
+    contexts,
+    onclick: submenuClicked,
+  });
+
+  const profiles = await getProfiles();
+
+  profiles.forEach(profile => {
+    browser.contextMenus.create({
+      parentId: "ff2mpv",
+      id: profile.id,
+      title: profile.name,
+      contexts,
+      onclick: submenuClicked,
+    })
   });
 
   browser.browserAction.onClicked.addListener((tab) => {
-      ff2mpv(tab.url);
+    ff2mpv(tab.url);
   });
 });
---
+++ options/options.js
@@ -0,0 +1,142 @@
+(() => {
+  const profilesWrapper = document.getElementById('profiles-wrapper');
+  const addButton = document.getElementById('add');
+  const PROFILES = 'profiles';
+
+  const getProfiles = async () => {
+    return (await browser.storage.sync.get(PROFILES))[PROFILES] || [];
+  };
+
+  const saveProfile = async (profile) => {
+    const storedProfiles = await getProfiles();
+    const existingProfile = storedProfiles.find(sp => sp.id === profile.id);
+    let updatedProfiles;
+
+    if (existingProfile) {
+      existingProfile.content = profile.content;
+      existingProfile.name = profile.name;
+
+      updatedProfiles = {
+        [PROFILES]: storedProfiles,
+      };
+    } else {
+      updatedProfiles = {
+        [PROFILES]: [
+          ...storedProfiles,
+          profile,
+        ]
+      };
+    }
+
+    return browser.storage.sync.set(updatedProfiles);
+  };
+
+  const deleteProfile = async (id) => {
+    const storedProfiles = await getProfiles();
+    const updatedProfiles = storedProfiles.filter(sp => sp.id !== id);
+
+    return browser.storage.sync.set({ [PROFILES]: updatedProfiles });
+  };
+
+  const onSaveProfile = (event) => {
+    const targetProfile = event.target.parentElement.parentElement;
+    const name = targetProfile.children[0].value;
+    const content = targetProfile.children[1].value
+      .trim().split('\n')
+      .map(line => line.trim());
+
+    // We need at least a name to save the profile
+    // or it won't have text on the menu context
+    if (!name) {
+      return;
+    }
+
+    const backgroundPage = browser.extension.getBackgroundPage();
+
+    // Save profile if it doesn't have an id
+    if (!targetProfile.dataset.id) {
+      targetProfile.dataset.id = crypto.randomUUID();
+      targetProfile.classList.remove('new-profile');
+
+      const id = targetProfile.dataset.id;
+      const profile = { id, name, content };
+      backgroundPage.createProfile(profile);
+
+      return saveProfile(profile);
+    }
+
+    const id = targetProfile.dataset.id;
+    const profile = { id, name, content };
+    backgroundPage.updateProfile(profile);
+
+    return saveProfile(profile);
+  };
+
+  const onDeleteProfile = (event) => {
+    const targetProfile = event.target.parentElement.parentElement;
+    const buttonsWrapper = targetProfile.children[2];
+    const id = targetProfile.dataset.id;
+
+    buttonsWrapper.children[0].removeEventListener('click', onSaveProfile);
+    buttonsWrapper.children[1].removeEventListener('click', onDeleteProfile);
+    profilesWrapper.removeChild(targetProfile);
+
+    if (id) {
+      const backgroundPage = browser.extension.getBackgroundPage();
+
+      backgroundPage.deleteProfile(id);
+
+      return deleteProfile(id);
+    }
+  };
+
+  const createProfile = ({ name = '', content = [], id }) => {
+    const profileDiv = document.createElement('div');
+    const profileName = document.createElement('input');
+    const profileContent = document.createElement('textArea');
+    const buttonsWrapper = document.createElement('div');
+    const saveButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+
+    if (id) {
+      profileDiv.dataset.id = id;
+    }
+
+    profileDiv.classList.add('profile');
+    buttonsWrapper.classList.add('buttons-wrapper');
+    saveButton.classList.add('button');
+    deleteButton.classList.add('button', 'delete-button');
+
+    profileName.placeholder = 'Profile Name';
+    profileContent.placeholder = 'mpv args (1 per line)';
+
+    saveButton.addEventListener('click', onSaveProfile);
+    deleteButton.addEventListener('click', onDeleteProfile);
+
+    profileContent.value = content.length ? content.join('\n') + '\n' : '';
+    profileName.value = name;
+
+    saveButton.textContent = 'save';
+    deleteButton.textContent = 'delete';
+
+    buttonsWrapper.append(saveButton, deleteButton);
+    profileDiv.append(profileName, profileContent, buttonsWrapper);
+
+    return profileDiv;
+  }
+
+  const load = async () => {
+    addButton.addEventListener('click', () => {
+      const emptyProfile = createProfile({});
+
+      emptyProfile.classList.add('new-profile');
+      profilesWrapper.appendChild(emptyProfile);
+    });
+
+    const profiles = await getProfiles();
+    const profileElements = profiles.map(pf => createProfile(pf));
+    profilesWrapper.replaceChildren(...profileElements);
+  };
+
+  window.addEventListener('load', load);
+})();

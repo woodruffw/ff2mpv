75d7bc53ba05078db9aad2bae5634b9868e74895
Eduardo D Sanchez <eduardo.daniel.s.m@gmail.com>
2024-01-28 12:18:21 -0500

    feat: Update context menu entry if no profiles (#115)
    * feat: Update context menu entry if no profiles
    
    * refactor: Define TITLE global variable

--- ff2mpv.js
+++ ff2mpv.js
@@ -5,6 +5,7 @@ function onError(error) {
 }
 
 const OPEN_VIDEO = 'openVideo';
+let TITLE;
 
 function ff2mpv(url, options = []) {
   browser.tabs.executeScript({
@@ -32,21 +33,58 @@ async function getOptions(id) {
 }
 
 async function submenuClicked(info) {
-  switch (info.parentMenuItemId) {
-    case "ff2mpv":
-      /* These should be mutually exclusive, but,
-         if they aren't, this is a reasonable priority.
-      */
-      url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl;
-      if (url) {
-        const options = await getOptions(info.menuItemId);
-        ff2mpv(url, options);
-      }
-    break;
+  if (info.parentMenuItemId === 'ff2mpv' || info.menuItemId === 'ff2mpv') {
+    /* These should be mutually exclusive, but,
+       if they aren't, this is a reasonable priority.
+    */
+    const url = info.linkUrl || info.srcUrl || info.selectionText || info.frameUrl;
+    if (url) {
+      const options = await getOptions(info.menuItemId);
+      ff2mpv(url, options);
+    }
   }
 }
 
-function createProfile(profile) {
+function changeToMultiEntries() {
+  // Remove single entry
+  browser.contextMenus.remove('ff2mpv');
+
+  // Add sub context menu
+  browser.contextMenus.create({
+    id: "ff2mpv",
+    title: "Profiles",
+    contexts,
+    onclick: submenuClicked,
+  });
+
+  browser.contextMenus.create({
+    parentId: "ff2mpv",
+    title: TITLE,
+    contexts,
+    onclick: submenuClicked,
+  });
+}
+
+function changeToSingleEntry() {
+  // Remove sub context menu
+  browser.contextMenus.remove('ff2mpv');
+
+  // Add single entry
+  browser.contextMenus.create({
+    id: "ff2mpv",
+    title: TITLE,
+    contexts,
+    onclick: submenuClicked,
+  });
+}
+
+async function createProfile(profile) {
+  const profiles = await getProfiles();
+
+  if (profiles.length === 0) {
+    changeToMultiEntries();
+  }
+
   browser.contextMenus.create({
     parentId: "ff2mpv",
     id: profile.id,
@@ -56,8 +94,15 @@ function createProfile(profile) {
   })
 }
 
-function deleteProfile(menuItemId) {
+async function deleteProfile(menuItemId) {
   browser.contextMenus.remove(menuItemId);
+
+  const profiles = (await getProfiles())
+    .filter(pf => pf.id !== menuItemId);
+
+  if (profiles.length === 0) {
+    changeToSingleEntry();
+  }
 }
 
 function updateProfile(profile) {
@@ -66,57 +111,49 @@ function updateProfile(profile) {
   });
 }
 
+browser.browserAction.onClicked.addListener((tab) => {
+  ff2mpv(tab.url);
+});
+
+// Messages sent with browser.runtime.sendMessage (https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/sendMessage) from external applications will be handle here.
+// ref: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessageExternal
+browser.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
+  if (!request) {
+    console.warn('No request in external message');
+    return;
+  }
+
+  const { type, url } = request;
+  console.debug('Request from:', sender);
+
+  switch (type) {
+    case OPEN_VIDEO:
+      ff2mpv(url);
+      return sendResponse('ok');
+    default:
+      console.warn('No handler for external type:', type);
+      return;
+  }
+});
+
 getOS().then(async (os) => {
-  var title = os == "win" ? "Play in MP&V" : "Play in MPV (&W)";
-
-  browser.contextMenus.create({
-    id: "ff2mpv",
-    title: "Profiles",
-    contexts,
-  });
-
-  // Default entry
-  browser.contextMenus.create({
-    parentId: "ff2mpv",
-    title,
-    contexts,
-    onclick: submenuClicked,
-  });
+  TITLE = os === "win" ? "Play in MP&V" : "Play in MPV (&W)";
 
   const profiles = await getProfiles();
 
-  profiles.forEach(profile => {
-    browser.contextMenus.create({
-      parentId: "ff2mpv",
-      id: profile.id,
-      title: profile.name,
-      contexts,
-      onclick: submenuClicked,
-    })
-  });
+  if (profiles.length === 0) {
+    changeToSingleEntry();
+  } else {
+    changeToMultiEntries();
 
-  browser.browserAction.onClicked.addListener((tab) => {
-    ff2mpv(tab.url);
-  });
-
-  // Messages sent with browser.runtime.sendMessage (https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/sendMessage) from external applications will be handle here.
-  // ref: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessageExternal
-  browser.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
-    if (!request) {
-      console.warn('No request in external message');
-      return;
-    }
-
-    const { type, url } = request;
-    console.debug('Request from:', sender);
-
-    switch (type) {
-      case OPEN_VIDEO:
-        ff2mpv(url);
-        return sendResponse('ok');
-      default:
-        console.warn('No handler for external type:', type);
-        return;
-    }
-  });
+    profiles.forEach(profile => {
+      browser.contextMenus.create({
+        parentId: "ff2mpv",
+        id: profile.id,
+        title: profile.name,
+        contexts,
+        onclick: submenuClicked,
+      })
+    });
+  }
 });
